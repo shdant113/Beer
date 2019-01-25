916aad48bed632b0b40cb4fc9c5da6f1d131709f
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-01-25 14:04:34 -0600

    baseline

---
+++ server.js
@@ -0,0 +1,19 @@
+require('./db');
+const express = require('express');
+const server = express();
+const request = require('superagent');
+	
+
+
+
+
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+server.use(express.static('public'));
+server.use(methodOverride('_method'));
+server.use(bodyParser.urlencoded({extended: false}));
+
+server.listen(3000, () => {
+	console.log('get to da choppa')
+})
