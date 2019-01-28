597441afd1ec98db739f477e29e0af86f7594f6e
Tim Mandel <timothy.mandel@gmail.com>
2019-01-28 14:15:09 -0600

    Seed data entered for breweries and users

---
+++ seed/seed.js
@@ -0,0 +1,65 @@
+const breweryData = [
+{
+	name: 'Goose Island',
+	city: 'Chicago',
+	state: 'Illinois',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Solemn Oath',
+	city: 'Naperville',
+	state: 'Illinois',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Samuel Adams',
+	city: 'Boston',
+	state: 'Massachussets',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Coors',
+	city: 'Golden',
+	state: 'Colorado',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Red Hook',
+	city: 'Seattle',
+	state: 'Washington',
+	creator: 'test',
+	beers: []
+}]
+
+const userData = [
+{
+	username: 'test',
+	password: 'test',
+	email: 'test@test.com',
+	city: 'Milwaukee',
+	state: 'Wisconsin',
+	fridge: []
+},
+{
+	username: 'fail',
+	password: 'fail',
+	email: 'fail@fail.com',
+	city: 'Mobile',
+	state: 'Alabama',
+	fridge: []
+},
+{
+	username: 'trial',
+	password: 'trial',
+	email: 'trial@trial.com',
+	city: 'Nashville',
+	state: 'Tennessee',
+	fridge: []
+}]
+
+module.exports = userData;
+module.exports = breweryData;
