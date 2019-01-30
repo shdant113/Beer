36ea7dd437da5425b2ca2f532c57fe07c6cc1c17
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-01-30 15:12:16 -0600

    seeding

--- seedBreweryData.js
+++ seedBreweryData.js
@@ -3,183 +3,351 @@ const breweryData = [
 	name: 'Goose Island',
 	city: 'Chicago',
 	state: 'Illinois',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Solemn Oath',
 	city: 'Naperville',
 	state: 'Illinois',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Samuel Adams',
 	city: 'Boston',
 	state: 'Massachussets',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Coors',
 	city: 'Golden',
 	state: 'Colorado',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Red Hook',
 	city: 'Seattle',
 	state: 'Washington',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Anheuser-Busch',
 	city: 'St. Louis',
 	state: "Missouri",
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'MillerCoors',
 	city: 'Chicago',
 	state: 'Illinois',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Heineken',
 	city: 'White Plains',
 	state: "New York",
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Pabst Brewing',
 	city: 'Los Angeles',
 	state: 'California',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Yuengling',
 	city: 'Pottsville',
 	state: 'Pennsylvania',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Sierra Nevada',
 	city: 'Chico',
 	state: 'California',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'New Belgium',
 	city: 'Fort Collins',
 	state: 'Colorado',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Founders',
 	city: 'Grand Rapids',
 	state: 'Michigan',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Bells',
 	city: 'Comstock',
 	state: 'Michigan',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Sapporo USA',
 	city: 'La Crosse',
 	state: 'Wisconsin',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Stone Brewing',
 	city: 'Escondido',
 	state: "California",
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Deschutes',
 	city: 'Bend',
 	state: 'Oregon',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Brooklyn Brewery',
 	city: 'Brooklyn',
 	state: 'New York',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Dogfish Head',
 	city: 'Milton',
 	state: 'Delaware',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'SweetWater',
 	city: 'Atlanta',
 	state: 'Georgia',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'New Glarus',
 	city: 'New Glarus',
 	state: 'Wisconsin',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Harpoon',
 	city: 'Boston',
 	state: 'Massachusetts',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Alaskan',
 	city: 'Juneau',
 	state: 'Alaska',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Great Lakes',
 	city: 'Cleveland',
 	state: 'Ohio',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'Abita',
 	city: 'Abita Springs',
 	state: 'Louisiana',
-	creator: 'test',
+	creator: 'admin',
 	beers: []
 },
 {
 	name: 'August Schell',
 	city: 'New Ulm',
 	state: 'Minnesota',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: 'Afterthought',
+	city: 'Lombard',
+	state: 'Illinois',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: 'Bent River Brewing Co',
+	city: 'Moline',
+	state: 'Illinois',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: 'Blue Nose Brewery',
+	city: 'Hodgkins',
+	state: 'Illinois',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: 'DESTIHIL',
+	city: 'Champaign',
+	state: 'Illinois',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: "Emmett's",
+	city: 'Downers Grove',
+	state: 'Illinois',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: 'Flapjack Brewery',
+	city: 'Berwyn',
+	state: 'Illinois',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: 'Horse Thief Hollow',
+	city: 'Chicago',
+	state: 'Illinois',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: 'Off Color Brewing',
+	city: 'Chicago',
+	state: 'Illinois',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: 'Bearded Owl Brewing',
+	city: 'Peoria',
+	state: 'Illinois',
+	creator: 'admin',
+	beers: []
+},
+{
+	name: 'Smylie Brothers',
+	city: 'Evanston',
+	state: 'Illinois',
 	creator: 'test',
 	beers: []
-}]
+},
+{
+	name: 'Danny Boy Beer Works',
+	city: 'Carmel',
+	state: 'Indiana',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Burn Em Brewing',
+	city: 'Michigan City',
+	state: 'Indiana',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Avondale Brewing Co',
+	city: 'Birmingham',
+	state: 'Alabama',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Band of Brothers Brewing Co',
+	city: 'Tuscaloosa',
+	state: 'Alabama',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Yellowhammer Brewery',
+	city: 'Hunstville',
+	state: 'Alabama',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'King Street Brewing Co',
+	city: 'Anchorage',
+	state: 'Alaska',
+	creator: 'test',
+	beers: []
+},
+{
+	name: '1912 Brewing',
+	city: 'Tucson',
+	state: 'Arizona',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Bad Water Brewing',
+	city: 'Scottsdale',
+	state: 'Arizona',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Dragoon Brewing Co',
+	city: 'Tuscon',
+	state: 'Arizona',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Diamond Bear Brewing',
+	city: 'Little Rock',
+	state: 'Arkansas',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Lost Forty Brewing',
+	city: 'Little Rock',
+	state: 'Arkansas',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'Rapps Barren Brewing Company',
+	city: 'Mountain Home',
+	state: 'Arkansas',
+	creator: 'test',
+	beers: []
+},
+{
+	name: 'BarrelHouse Brewing Co.',
+	city: 'Paso Roblems',
+	state: 'California',
+	creator: 'test',
+	beers: []
+},
+{
+	name: '',
+	city: '',
+	state: '',
+	creator: 'test',
+	beers: []
+},
 
 module.exports = breweryData;
